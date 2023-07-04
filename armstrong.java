61db7ce59bad6e80bba4657ead28f3986b336062
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-07-04 13:11:58 +0000

    done with armstrong number

---
+++ armstrong.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class armstrong {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+
+        armstrong(n);
+
+    }
+
+    static void armstrong(int n){
+        int sum = 0;
+        int original_number = n;
+        while(n > 0){
+        int num = n % 10;
+        sum = sum + (num * num * num);
+        n = n/10;
+        }
+
+        if(sum == original_number){
+            System.out.println("armstrong number.....");
+        }else{
+            System.out.println("not an armstrong number.....");
+        }
+
+
+
+    }
+}

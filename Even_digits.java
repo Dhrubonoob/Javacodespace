5c42ccd2907f076d860d6d9c61d4590590bf1745
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-07-13 15:52:59 +0000

    done

--- Even_digits.java
+++ Even_digits.java
@@ -1,14 +1,15 @@
 public class Even_digits {
     public static void main(String[] args) {
-        int[] nums = {12,345,2,6,7896};
+        int[] nums = { 12, 345, 2, 6, 7896 };
 
         System.out.println(findNumbers(nums));
     }
-    static int findNumbers(int[] nums){
+
+    static int findNumbers(int[] nums) {
         int count = 0;
 
-        for(int num : nums){
-            if(checkEven(num)){
+        for (int num : nums) {
+            if (checkEven(num)) {
                 count++;
             }
         }
@@ -16,19 +17,19 @@ public class Even_digits {
         return count;
     }
 
-    static boolean checkEven(int num){
+    static boolean checkEven(int num) {
         int noofDigits = digitsCount(num);
-        if(noofDigits % 2 == 0){
+        if (noofDigits % 2 == 0) {
             return true;
         }
         return false;
     }
 
-    static int digitsCount(int num){
+    static int digitsCount(int num) {
         int count = 0;
-        while(num > 0){
+        while (num > 0) {
             count++;
-            num = num/10;
+            num = num / 10;
         }
         return count;
     }

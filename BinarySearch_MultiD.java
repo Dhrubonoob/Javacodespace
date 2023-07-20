b26d8d356ba2af03adcbeb117df1e268893ed58d
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-07-20 15:36:40 +0000

    new modified

--- BinarySearch_MultiD.java
+++ BinarySearch_MultiD.java
@@ -3,15 +3,30 @@ import java.util.*;
 
 public class BinarySearch_MultiD {
     public static void main(String[] args) {
-        int[][] matrix = {
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,10,11,12},
-            {13,14,15,16}
-        };
+       
+       
+        // int[][] matrix = {
+        //     {1,2,3,4},
+        //     {5,6,7,8},
+        //     {9,10,11,12},
+        //     {13,14,15,16}
+        // };
 
-        int target = 11;
+        // int target = 11;
        // int ans = search(matrix, target);
+
+       
+       //taking user input in the terminal
+       Scanner sc =new Scanner(System.in);
+        int[][] matrix = new int[4][4];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                matrix[r][c] = sc.nextInt();
+            }
+        }
+
+        int target = sc.nextInt();
+
         System.out.println(Arrays.toString(search(matrix, target)));
     }
 

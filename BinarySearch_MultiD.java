77e98988d9a88a9a8f6773af527732dbd91d996b
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-07-20 15:17:46 +0000

    new

--- BinarySearch_MultiD.java
+++ BinarySearch_MultiD.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class BinarySearch_MultiD {
     public static void main(String[] args) {
         int[][] matrix = {
-            {10,20,30,40},
-            {15,25,35,45},
-            {28,29,37,49},
-            {33,34,38,50}
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
         };
 
-        int target = 28;
+        int target = 11;
        // int ans = search(matrix, target);
         System.out.println(Arrays.toString(search(matrix, target)));
     }

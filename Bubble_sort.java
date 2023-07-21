1d72777b638f4016e3614750ac8df81bb73b4c33
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-07-21 14:02:10 +0000

    sorted

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 public class Bubble_sort {
     public static void main(String[] args) {
-        int[] arr = {23,44,30,2,10};
+        int[] arr = { 23, 44, 30, 2, 10 };
 
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void bubbleSort(int[] arr){
+    static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j=1;j<arr.length-i;j++){
-                if (arr[j] < arr[j-1]) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
         }

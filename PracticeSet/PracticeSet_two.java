528046e9dc427276346ec4c50db8b37f0e99a663
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-07-27 10:36:03 +0000

    done

---
+++ PracticeSet/PracticeSet_two.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+public class PracticeSet_two {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        int target = sc.nextInt();
+      //  int ans = Multidimensional_searching(arr, target);
+
+        System.out.println(Arrays.toString(Multidimensional_searching(arr, target)));
+    }
+
+    static int[] Multidimensional_searching(int[][] arr, int target){
+
+        int r = 0;
+        int c = arr.length - 1;
+
+        while (r < arr.length && c >=0 ) {
+            if (arr[r][c] == target) {
+                return new int[]{r , c};
+            }
+
+            if (arr[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+
+        return new int[]{-1, -1};
+
+    }
+}

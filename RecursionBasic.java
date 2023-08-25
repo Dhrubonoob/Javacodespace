f54af2232915199dda5b7ab00fe2232395ef91a7
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-08-25 12:49:06 +0000

    sum, factorials, sum of digits, product of digits

--- RecursionBasic.java
+++ RecursionBasic.java
@@ -6,7 +6,8 @@ public class RecursionBasic {
         // System.out.println(funcRev(5));
         // System.out.println(factorial(5));
         // System.out.println(sum(5));
-        System.out.println(sumOfDigits(45812));
+        // System.out.println(sumOfDigits(45812));
+        System.out.println(productOftheDigits(123456));
     }
 
     static int func(int n){
@@ -47,5 +48,13 @@ public class RecursionBasic {
         }
         int rem = n % 10;
         return sumOfDigits(n/10) + rem;  
+    }
+
+    static int productOftheDigits(int n){
+        if(n<=1){
+            return 1;
+        }
+        int rem = n % 10;
+        return productOftheDigits(n/10) * rem;
     }
 }

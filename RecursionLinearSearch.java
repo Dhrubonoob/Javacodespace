74863abbe7cde47c202596f86a6e5f95ef76f7af
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-08-26 17:54:24 +0000

    linear search with recursion

--- RecursionLinearSearch.java
+++ RecursionLinearSearch.java
@@ -1,9 +1,14 @@
 public class RecursionLinearSearch {
     public static void main(String[] args) {
         int[] arr = {4, 2, 3, 5, 6};
-        System.out.println(search(arr, 6, 0, 0));
+        System.out.println(search(arr, 5, 0, 0));
     }
     static int search(int[] arr, int target, int ind, int c){
+
+        if(ind == arr.length){
+            return -1;
+        }
+
         if (arr[ind] == target) {
             // return c;
             return ind;

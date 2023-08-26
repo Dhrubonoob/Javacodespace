673e30cacb430a8a7f8d6bf64c127025f4c003aa
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-08-26 18:55:45 +0000

    search for single elements and repeating elements

--- RecursionLinearSearch.java
+++ RecursionLinearSearch.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class RecursionLinearSearch {
     public static void main(String[] args) {
-        int[] arr = {4, 2, 3, 5, 6};
-        System.out.println(search(arr, 5, 0, 0));
+        int[] arr = {4, 2, 3, 5, 6, 5};
+        // System.out.println(search(arr, 5, 0, 0));
+        searchList(arr, 5, 0);
+        System.out.println(list);
     }
     static int search(int[] arr, int target, int ind, int c){
 
@@ -16,4 +20,18 @@ public class RecursionLinearSearch {
 
         return search(arr, target, ind+1,c+1);
     }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void searchList(int[] arr, int target, int ind){
+        if(ind == arr.length){
+            return;
+        }
+
+        if(arr[ind] == target){
+            list.add(ind);
+        }
+
+        searchList(arr, target, ind+1);
+    }
 }
+ 

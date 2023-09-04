c769638503e4b6153b93c81019a5a7592ac0a40f
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-09-04 07:36:27 +0000

    subset

--- RecursionSubsetTwo.java
+++ RecursionSubsetTwo.java
@@ -1,6 +1,7 @@
 public class RecursionSubsetTwo {
     public static void main(String[] args) {
         System.out.println(skip2("abcdabsae"));
+        skip("", "adhhasd");
     }
 
     static String skip2(String up){
@@ -15,5 +16,20 @@ public class RecursionSubsetTwo {
         }else{
             return ch + skip2(up.substring(1));
         }
+    }
+
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+
+        if(ch == 'a'){
+            skip(p, up.substring(1));
+        }else{
+            skip(p+ch, up.substring(1));
+        }
+
     }
 }

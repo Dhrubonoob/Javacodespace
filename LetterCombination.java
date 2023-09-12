126c8352f6e29beefb310653088f2912b1cb7f60
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-09-12 04:55:04 +0000

    letter combinations with recursion

--- LetterCombination.java
+++ LetterCombination.java
@@ -1,8 +1,12 @@
-// import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class LetterCombination {
     public static void main(String[] args) {
         letter("", "2");
+        ArrayList<String> res = new ArrayList<>();
+        res = letterRet("", "12");
+        System.out.println(res);
     }
 
     static void letter(String p, String up){
@@ -18,4 +22,21 @@ public class LetterCombination {
         }
         
     }    
+
+    static ArrayList<String> letterRet(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int digit = up.charAt(0) - '0';
+
+        ArrayList<String> out = new ArrayList<>();
+
+        for(int  i = (digit-1)*3; i<digit*3; i++){
+            char ch = (char)('a'+i);
+            out.addAll(letterRet(p+ch, up.substring(1)));
+        }
+        return out;
+    }
 }

b1c18680b16aea81c6a181094743ab232f4585a6
Dhrubonoob <112429399+Dhrubonoob@users.noreply.github.com>
2023-09-12 05:47:01 +0000

    dice throw

---
+++ DiceThrow.java
@@ -0,0 +1,16 @@
+public class DiceThrow {
+    public static void main(String[] args) {
+        diceThrow("", 4);
+    }
+
+    static void diceThrow(String p, int target){
+        if(target == 0){
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <=6 && i<=target; i++) {
+            diceThrow(p+i, target-i);
+        }
+    }
+}
